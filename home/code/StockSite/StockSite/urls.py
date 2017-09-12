a9e4adbdc5e1fa67adada536fc230a6ded2a594a
mattsanner <mwsanner@ksu.edu>
2017-09-12 00:17:40 -0500

    adding to dashboard functionality, response accounts for user authentication status, redirects if not signed in

--- home/code/StockSite/StockSite/urls.py
+++ home/code/StockSite/StockSite/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('allauth.urls')),
+    url(r'^accounts/', include('allauth.urls'), name='allauth'),
+    url(r'', include('dashboard.urls'))
 ]
--- home/code/StockSite/dashboard/views.py
+++ home/code/StockSite/dashboard/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.urls import reverse
+from allauth.account import urls
 
 # Create your views here.
+def home(request):
+    if(request.user.is_authenticated):
+        return HttpResponse("Signed in Dashboard page woo!")
+    else:
+        return redirect(reverse('account_login'))
